597ea61d24d9d23f28374464912d893f53d70041
Assil Jaby <73783095+AltF4irl@users.noreply.github.com>
2024-04-02 00:21:13 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express()
 
 const errorHandler = (err, req, res, next) => {
     console.log(err.message)
-
     if(err.name === 'CastError') {
-        return res.status(400).send({ error: 'Malformatted id'})
+        return res.status(400).send({ error: 'Malformatted id' })
+    } else if (err.name === 'ValidationError') {
+        return res.status(400).json({ error: err.message })
     }
-
     next(err)
 }
 
@@ -20,12 +20,12 @@ app.use(express.static('dist'))
 app.use(express.json())
 
 
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (req, res) => {
-    res.send("<h1> Hello There!</h1>")
+    res.send('<h1> Hello There!</h1>')
 })
 
 app.get('/api/contacts', (req, res) => {
@@ -48,9 +48,9 @@ app.get('/api/contacts/:id', (req, res, next) => {
         } else {
             res.status(404).end()
         }
-        
+
     })
-    .catch(err => next(err))   
+    .catch(err => next(err))
 })
 
 app.post('/api/contacts', (req, res, next) => {
@@ -67,38 +67,40 @@ app.post('/api/contacts', (req, res, next) => {
         number: body.number
     })
 
-    Contact.find({name: body.name})
+    Contact.find({ name: body.name })
     .then(queryResult => {
         console.log(queryResult)
         if (queryResult.length === 0) {
-            contact.save().then(queryResult => res.json(queryResult))
+            contact.save()
+            .then(queryResult => res.json(queryResult))
+            .catch(err => next(err))
         } else {
             return res.status(400).json({
                 error: `${body.name} exists`
             })
         }
     })
-    .catch(err => next(err))
 })
 
 app.delete('/api/contacts/:id',  (req, res, next) => {
-    Contact.findOneAndDelete({_id: req.params.id})
+    Contact.findOneAndDelete({ _id: req.params.id })
     .then(deletedContact => res.json(deletedContact))
-    .catch(err => next(err))  
-})
-
-app.put('/api/contacts/:id', (req, res, next) => {
-    const currentContact = req.body 
-
-    console.log(currentContact, "ccurrent");
-
-    Contact.findOneAndUpdate({_id: req.params.id}, currentContact, {new: true})
-    .then(updatedContact => {
-        res.status(201).json(updatedContact)
-    }) 
     .catch(err => next(err))
 })
 
+app.put('/api/contacts/:id', (req, res, next) => {
+    const currentContact = req.body
+
+    console.log(currentContact, 'ccurrent')
+
+    Contact.findOneAndUpdate({ _id: req.params.id }, currentContact, { new: true, runValidators: true, context: 'query' })
+    .then(updatedContact => {
+        res.status(201).json(updatedContact)
+    })
+    .catch(err => next(err))
+})
+
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
 // const PORT = 3001
 app.listen(PORT, () => {
--- models/contact.js
+++ models/contact.js
@@ -1,24 +1,37 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 console.log('Connecting to', url)
 
 mongoose.connect(url)
 .then(result => {
-    console.log("Connected to MongoDB")
+    console.log('Connected to MongoDB', result)
 })
 .catch(err => {
-    console.log("Error connecting to MongoDB:", err.message)
+    console.log('Error connecting to MongoDB:', err.message)
 })
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 9,
+        required: [true, 'User phone number required'],
+        validate: {
+            validator: (v) => /^\d{2,3}-\d+$/.test(v),
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    }
 })
 
 contactSchema.set('toJSON', {
-    transform: (doc, returnedContact) => {
+    transform: (_doc, returnedContact) => {
         returnedContact.id = returnedContact._id.toString()
         delete returnedContact._id
         delete returnedContact.__v
